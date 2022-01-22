3994306a5022ef8d7f02b4ee5331524d3dd6db4f
Altamash <gitfromgeeks@gmail.com>
2022-01-22 17:26:12 +0530

    little more

---
+++ MongoAPI/router/router.go
@@ -0,0 +1,13 @@
+package router
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.createMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteAllMovie", controller.DeleteAllMovies).Methods("DELETE")
+
+	return router
+}
