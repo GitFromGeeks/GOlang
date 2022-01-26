7454c92bc0e188f06f2cafce634f4c3a4229bf7b
Altamash <gitfromgeeks@gmail.com>
2022-01-27 00:17:07 +0530

    one more pattern

--- 2 Patterns/21NumberPattern.go
+++ 2 Patterns/21NumberPattern.go
@@ -19,7 +19,11 @@ func pattern(n int) {
 	fmt.Println("your patter is :")
 	for i := 0; i < 2*n-1; i++ {
 		for j := 0; j < 2*n-1; j++ {
-			fmt.Print(n)
+			if i >= 1 && j >= 1 && i != 2*n-2 && j != 2*n-2 {
+				fmt.Print(n - 1)
+			} else {
+				fmt.Print(n)
+			}
 		}
 		fmt.Println("")
 	}
