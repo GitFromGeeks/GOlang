d0262e32ed36ed7641f947a8c355e314e06471bb
Altamash <gitfromgeeks@gmail.com>
2022-02-01 23:00:43 +0530

    nothing change

--- 3 Func & Array/6FindDuplicate.go
+++ 3 Func & Array/6FindDuplicate.go
@@ -10,8 +10,18 @@ func main() {
 		fmt.Scanln(&n)
 		list = append(list, n)
 	}
+	fmt.Println("Your list of numbers", list)
+	fmt.Println(findDuplicate(list))
+}
+
+func findDuplicate(list []int) []int {
 	newList := []int{}
-	fmt.Println(newList)
+	for i := range list {
+		x := list[i] % len(list)
+		list[x] = list[x] + len(list)
+	}
+
+	return newList
 }
 
 // 1 1 2 4 5 6 6
--- 4 Searching & Sorting/1BinarySort.go
+++ 4 Searching & Sorting/1BinarySort.go
@@ -1,2 +1,19 @@
 // 1010110101
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Binary Sorting")
+	list := []int{1, 0, 1, 1, 0, 0, 1, 1, 0}
+	fmt.Print(binarySorting(list))
+}
+func binarySorting(list []int) []int {
+	newList := []int{}
+	for i := range list {
+
+	}
+	return newList
+}
+
 // out 0000111111
